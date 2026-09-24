ad37d5818fd00e8bc434dfaf9474588a600a28d7
molomperformance-blip <molom.performance@gmail.com>
2026-09-24 16:13:38 +0200

    Bump PWA cache for grouped checklist update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='molom-gear-v6'; const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='molom-gear-v7'; const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))})
